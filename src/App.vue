6695936f9bc5cbdf74a18b6b8515ab289b0a9d8a
Jesús Velázquez <jebus.velazquez@gmail.com>
2020-08-30 18:52:02 -0500

    Set up initial design

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,60 @@
 <template>
-  <div id="app" class="bg-gray-300">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="w-full h-screen container bg-cover text-white p-2">
+    <div class="h-full mx-auto max-w-sm px-4 bg-purple-900 bg-opacity-50 rounded">
+      <h1 class="py-4 text-3xl font-bold text-center "> Tip calculator </h1>
+      <div class="pt-6 pb-4 flex justify-center items-center">
+        <div class="mr-2 text-2xl font-semibold">$</div>
+        <input class="w-40 px-3 py-2 rounded text-right text-xl shadow appearance-none border text-purple-900 leading-tight focus:outline-none focus:shadow-outline font-semibold" type="number" placeholder="Check total"/>
+      </div>
+      <Slider />
+      <Slider />
+      <div class="pb-2 flex justify-center items-center">
+        <div class="mr-2 w-8/12 "></div>
+        <div class="mr-2 text-xl w-4/12 text-center">Per person</div>
+      </div>
+      <div class="pb-4 flex justify-center items-center">
+        <div class="mr-2 text-2xl font-semibold w-8/12 text-right">Sub total</div>
+        <input class="w-4/12 px-3 py-2 rounded text-right text-xl shadow appearance-none border text-purple-900 leading-tight focus:outline-none focus:shadow-outline font-semibold" type="number" placeholder="0"/>
+      </div>
+      <div class="pb-4 flex justify-center items-center">
+        <div class="mr-2 text-2xl font-semibold w-8/12 text-right">Tip </div>
+        <input class="w-4/12 px-3 py-2 rounded text-right text-xl shadow appearance-none border text-purple-900 leading-tight focus:outline-none focus:shadow-outline font-semibold" type="number" placeholder="0"/>
+      </div>
+      <div class="pb-4 flex justify-center items-center">
+        <div class="mr-2 text-2xl font-semibold w-8/12 text-right">Total</div>
+        <input class="w-4/12 px-3 py-2 rounded text-right text-xl shadow appearance-none border text-purple-900 leading-tight focus:outline-none focus:shadow-outline font-semibold" type="number" placeholder="0"/>
+      </div>
+      <div class="text-center px-2 mt-4 mb-2 text-xs uppercase">Photo by <a href="https://unsplash.com/@dosejuice?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Dose Juice</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Slider from '@/components/Slider'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Slider
   }
 }
+//
 </script>
 
 <style>
 @import './assets/styles.css';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url(./assets/bg.jpg);
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
 }
 </style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="py-4">
+    <div class="flex justify-center items-center">
+      <svg class="w-12 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+      </svg>
+      <div class="mr-2">
+        <label class="font-semibold text-xl mb-2"> Tip % </label>
+        <input class="slider w-full" type="range" min="1" max="50" value="10">
+      </div>
+      <input class="w-20 px-3 py-2 rounded text-right text-xl shadow appearance-none border text-gray-700 leading-tight focus:outline-none focus:shadow-outline font-semibold" type="number" placeholder="0"/>
+      <div class="ml-2 text-2xl font-semibold">%</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* The slider itself */
+.slider {
+    -webkit-appearance: none;  /* Override default CSS styles */
+    appearance: none;
+    height: 10px; /* Specified height */
+    background: #d3d3d3; /* Grey background */
+    outline: none; /* Remove outline */
+    opacity: 01; /* Set transparency (for mouse-over effects on hover) */
+    -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+    transition: opacity .2s;
+}
+/* Mouse-over effects */
+.slider:hover {
+    opacity: 1; /* Fully shown on mouse-over */
+}
+/* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 20px; /* Set a specific slider handle width */
+    height: 20px; /* Slider handle height */
+    background: #f59b25; 
+    cursor: pointer; /* Cursor on hover */
+}
+.slider::-moz-range-thumb {
+    width: 12px; /* Set a specific slider handle width */
+    height: 12px; /* Slider handle height */
+    background: #f59b25;
+    cursor: pointer; /* Cursor on hover */
+}
+
+</style>
